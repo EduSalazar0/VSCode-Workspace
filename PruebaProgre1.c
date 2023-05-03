82a62879b5c02d30ef9db1edcbc8325ae69db992
Eduardo Salazar <edutebansalazar0@gmail.com>
2023-05-03 15:29:01 -0500

    Creacion de while para convertir el numero en hexadecimal

--- PruebaProgre1.c
+++ PruebaProgre1.c
@@ -2,4 +2,10 @@
 int main(int argc, char *argv[]){
     int decimal;
     char hex[16] = {'1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+    printf("Escribe un numero en decimal\n");
+    scanf("%d",&decimal);
+    while(decimal >= 16){
+        decimal = decimal/16;
+        
+    }
 }
