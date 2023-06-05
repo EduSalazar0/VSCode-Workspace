65dedff181e72b5b111259aa8b7f9cae5eec0c18
Eduardo Salazar <edutebansalazar0@gmail.com>
2023-06-05 15:21:08 -0500

    Creaccion de funcion matriz

---
+++ PruebaProgreso2/Preuba.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int crearmatriz(int matriz[3][23]){
+    
+    for(int i=0;i<3;i++){
+        for(int j=0;j<23;j++){
+            matriz[i][j]=rand()%11;
+            printf(" %d ",matriz[i][j]);
+        }
+    }
+    printf("\n");
+    return matriz;
+}
+
+
+int main(){
+    int X[3][23];
+    crearmatriz(X);
+
+
+    return 0;
+}
